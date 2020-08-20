bc6b6376b14d17fea6163e458e62ee6423f1ba98
aquaswim <bayusetiawan37@gmail.com>
2020-08-20 10:54:54 +0700

    adding simple providers service

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,9 +1,11 @@
 import { Application } from '../declarations';
 import users from './users/users.service';
 import series from './series/series.service';
+import providers from './providers/providers.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
   app.configure(series);
+  app.configure(providers);
 }
