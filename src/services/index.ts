d7c501e4bd28f76a54d57c097044ef924c44d104
aquaswim <bayusetiawan37@gmail.com>
2020-08-20 10:57:52 +0700

    adding simple projects service

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,12 @@ import { Application } from '../declarations';
 import users from './users/users.service';
 import series from './series/series.service';
 import providers from './providers/providers.service';
+import projects from './projects/projects.service';
 // Don't remove this comment. It's needed to format import lines nicely.
 
 export default function (app: Application): void {
   app.configure(users);
   app.configure(series);
   app.configure(providers);
+  app.configure(projects);
 }
---
+++ src/services/projects/projects.service.ts
@@ -0,0 +1,26 @@
+// Initializes the `projects` service on path `/projects`
+import { ServiceAddons } from '@feathersjs/feathers';
+import { Application } from '../../declarations';
+import { Projects } from './projects.class';
+import hooks from './projects.hooks';
+
+// Add this service to the service type index
+declare module '../../declarations' {
+  interface ServiceTypes {
+    'projects': Projects & ServiceAddons<any>;
+  }
+}
+
+export default function (app: Application): void {
+  const options = {
+    paginate: app.get('paginate')
+  };
+
+  // Initialize our service with any options it requires
+  app.use('/projects', new Projects(options, app));
+
+  // Get our initialized service so that we can register hooks
+  const service = app.service('projects');
+
+  service.hooks(hooks);
+}
